12ee1bc328517229f3cec68cb1e18b92c9fa0c29
Space Expanse <97041967+spaceexpanse@users.noreply.github.com>
2022-06-10 13:56:21 +0200

    Change initialSubsidy

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -136,7 +136,7 @@ public:
         switch (type)
         {
             case Fork::POST_ICO:
-                return height >= 2280;  /* Set to 1 day */
+                return height >= 120;  /* Set to 1 hours */
             default:
                 assert (false);
         }
